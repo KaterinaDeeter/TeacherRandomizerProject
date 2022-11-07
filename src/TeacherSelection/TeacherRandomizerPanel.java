fef31842ec3597ada74c03ca7db0437e18f8e4d2
Katerina Deeter <ktdeeter@gmail.com>
2022-11-07 12:03:56 -0700

    Added to TeacherSelect (selection of teacher, based on the school list from SchoolOptions.txt). Will later be adding comments on the thought process

--- src/TeacherSelection/TeacherRandomizerPanel.java
+++ src/TeacherSelection/TeacherRandomizerPanel.java
@@ -8,24 +8,30 @@ public class TeacherRandomizerPanel extends JPanel
 {
     private JLabel intro;
     private JLabel results;
-    private JButton fullRandom;
-    private JComboBox<String> gradeSelect;
-    private String[] gradeStrings = {"K", "1st" , "2nd","3rd","4th","5th","6th","7th","8th","9th","10th","11th", "12th"};
+//    private JButton fullRandom;
+    private JComboBox<String> schoolSelect;
+    private String[] schoolStrings = {"Mesa High School", "Red Mountain Ranch Elementary", "Andersen Elementary" ,
+            "Kyrene Traditional Academy", "Highland Junor High", "Kyrene de las Manitas Innovation Academy",
+            "Hamilton High", "Anderson Junior High", "Arizona College Prep High School", "Kino Junior High",
+            "Kyrene de la Mariposa Computer Science Academy", "Chandler High School",
+            "Franklin at Alma Elementary School", "Tempe High School", "Desert Vista High School",
+            "Desert Ridge High School", "Kyrene de los Niños", "Gilbert High School", "Perry High School"};
     String selection;
     ButtonListener listener = new ButtonListener();
+    TeacherSelect selector = new TeacherSelect();
 
     public TeacherRandomizerPanel()
     {
         intro = new JLabel("Welcome, select an option");
         add(intro);
 
-        fullRandom = new JButton("Fully Randomized");
-        fullRandom.addActionListener(listener);
-        add(fullRandom);
+//        fullRandom = new JButton("Fully Randomized");
+//        fullRandom.addActionListener(listener);
+//        add(fullRandom);
 
-        gradeSelect = new JComboBox<>(gradeStrings);
-        gradeSelect.addActionListener(listener);
-        add(gradeSelect);
+        schoolSelect = new JComboBox<>(schoolStrings);
+        schoolSelect.addActionListener(listener);
+        add(schoolSelect);
 
         results = new JLabel("Hello");
         add(results);
@@ -37,14 +43,16 @@ public class TeacherRandomizerPanel extends JPanel
     {
         public void actionPerformed (ActionEvent event)
         {
-            if (event.getSource() == fullRandom)
+//            if (event.getSource() == fullRandom)
+//            {
+//                results.setText("Yee Haw");
+//            }
+            //else
+            if (event.getSource() == schoolSelect)
             {
-                results.setText("Yee Haw");
-            }
-            else if (event.getSource() == gradeSelect)
-            {
-                selection = (String) gradeSelect.getSelectedItem();
+                selection = (String) schoolSelect.getSelectedItem();
                 results.setText(selection);
+                selector.setSchoolName(selection);
             }
         }
     }
--- src/TeacherSelection/TeacherSelect.java
+++ src/TeacherSelection/TeacherSelect.java
@@ -1,9 +1,327 @@
 package TeacherSelection;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class TeacherSelect
 {
-    //This should try reading SchoolOptions.txt. Look for 'The School Is' and set school to that option
-    //Once it receives a school option, then we can make it access the "school database". Once it accesses school
-    //then it can select a grade (randomizer, done at some point), then teacher...
-    //Or just a dropdown for each school but that will be more user work. .equalsIgnoreSpaces would probably be helpful
+    private String schoolName;
+    private String schoolLevel;
+    private String teacher;
+    private int grade;
+    Random generator = new Random();
+
+    public TeacherSelect()
+    {
+        schoolName = "undefined";
+        grade = 0;
+        schoolLevel = "undefined";
+        teacher = "undefined";
+    }
+
+    public void setSchoolName(String schoolName)
+    {
+        this.schoolName = schoolName;
+    }
+    private void setSchoolandGradeLevel() //I removed middle and high school here, because this will exclusively be STEM focused
+    {
+        if (isElementaryTypeA())
+        {
+            grade = generator.nextInt(7); //K-6
+        }
+        else if(isElementaryTypeB())
+        {
+            grade = generator.nextInt(6); //K-5
+        }
+        else if(schoolName.equals("Kyrene de las Manitas Innovation Academy"))
+        {
+        }
+        else if (schoolName.equals("Kyrene Traditional Academy"))
+        {
+        }
+    }
+
+    private boolean isElementaryTypeA()
+    {
+        if (schoolName.equals("Red Mountain Ranch Elementary") || schoolName.equals("Franklin at Alma Elementary School"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    private boolean isElementaryTypeB()
+    {
+        if (schoolName.equals("Andersen Elementary") || schoolName.equals("Kyrene de la Mariposa Computer Science Academy") ||
+            schoolName.equals("Kyrene de los Niños"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+
+    public void teacherGenerator() //ADD to this one as we continue
+    {
+        if(schoolName.equals("Red Mountain Ranch Elementary"))
+        {
+            redMountainRanchTeacherSelector();
+        }
+        else if(schoolName.equals("Franklin at Alma Elementary School"))
+        {
+            franklinAtAlmaTeacherSelector();
+        }
+        else if(schoolName.equals("Andersen Elementary"))
+        {
+            andersonElementaryTeacherSelector();
+        }
+        else if(schoolName.equals("Kyrene de la Mariposa Computer Science Academy"))
+        {
+            kdlMCSAElementaryTeacherSelector();
+        }
+        else if (schoolName.equals("Kyrene de los Niños"))
+        {
+            kyreneDeLosNinosElementaryTeacherSelector();
+        }
+    }
+    private void redMountainRanchTeacherSelector() //DONE
+    {
+        if (grade == 0) //Done
+        {
+            String[] teacherArray = {"Alysha Freza", "Gretchen Rodeffer", "Nicole McBrien"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+        else if (grade == 1) //Done
+        {
+            String[] teacherArray = {"Sabrina Hill", "Ashlea Bowden", "Janet Smolka"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+
+        }
+        else if (grade == 2) //Done
+        {
+            String[] teacherArray = {"Hannah Saunders", "Krista Fox", "Kaitlyn Moister"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+        else if (grade == 3) //Done
+        {
+            String[] teacherArray = {"Kelly Argos", "Mackenzi Kugler", "Wendy Hall"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+        else if (grade == 4) //Done
+        {
+            String[] teacherArray = {"Melissa Mashore", "Jennifer Bagnasco"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(2));
+        }
+        else if (grade == 5) //Done
+        {
+            String[] teacherArray = {"Dana Moore", "Beth Garcia"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(2));
+        }
+        else if (grade == 6) //Done
+        {
+            String[] teacherArray = {"Ms. Gardner", "Mr.Gibson" , "Mrs.Lewis"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+    }
+    private void franklinAtAlmaTeacherSelector() //DONE
+    {
+        if (grade == 0)
+        {
+            String[] teacherArray = {"Merrie Ehrke", "Camilla Moore", "Grace Plyler"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+        else if (grade == 1)
+        {
+            String[] teacherArray = {"Laila Raether", "Monessa Clinkenbeard"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(2));
+
+        }
+        else if (grade == 2)
+        {
+            String[] teacherArray = {"Jennifer Gummow", "Theresa Wilmes"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(2));
+        }
+        else if (grade == 3)
+        {
+            String[] teacherArray = {"Lisa Myers", "Patricia Traughber"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(2));
+        }
+        else if (grade == 4)
+        {
+            String[] teacherArray = {"Brittney Asamane", "Carol Reed"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(2));
+        }
+        else if (grade == 5)
+        {
+            String[] teacherArray = {"Dee Averyt", "Marissa Fenton"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(2));
+        }
+        else if (grade == 6)
+        {
+            String[] teacherArray = {"Lisa Beavers", "Melissa Standage"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(2));
+        }
+    }
+    private void andersonElementaryTeacherSelector() //Done
+    {
+        if (grade == 0)
+        {
+            String[] teacherArray = {"Miss Crawford", "Mrs. Padilla", "Mrs.Wagner"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+        else if(grade == 1)
+        {
+            String[] teacherArray = {"Mrs. Ball", "Mrs. Chandler", "Mrs. Quinn"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+        else if(grade == 2)
+        {
+            String[] teacherArray = {"Ms. Casillas", "Ms. Green", "Mrs. Wilson"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+        else if(grade == 3)
+        {
+            String[] teacherArray = {"Miss Jaffe", "Mrs. Miller", "Mrs. Richardson"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+        else if(grade == 4)
+        {
+            String[] teacherArray = {"Ms. Coady", "Miss Dow", "Mrs. Marlar"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+        else if(grade == 5)
+        {
+            String[] teacherArray = {"Mrs. Dougherty", "Mrs. McClain", " Mrs. Williams"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+
+    }
+    private void kdlMCSAElementaryTeacherSelector() // Done
+    {
+        if (grade == 0)
+        {
+            String[] teacherArray = {"Ms. Adrian", "Ms. Rees", "Ms. Sanson"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+        else if(grade == 1)
+        {
+            String[] teacherArray = {"Ms. Gallagher", "Ms. Scheitlin", "Ms.Wilson"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+        else if(grade == 2)
+        {
+            String[] teacherArray = {"Ms. Feeley", "Mrs. Harshfield", "Mrs. Jennewein"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+        else if(grade == 3)
+        {
+            String[] teacherArray = {"Ms. Bustamante", "Mrs. Montiel", "Mrs. Russo"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+        else if(grade == 4)
+        {
+            String[] teacherArray = {"Mrs. Ballatore", "Ms. Barber", "Mrs. Johnson"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+        else if(grade == 5)
+        {
+            String[] teacherArray = {"Mrs. Languren", "Ms. Smith", "Ms. Stupica"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+    }
+    private void kyreneDeLosNinosElementaryTeacherSelector() //DONE
+    {
+        if (grade == 0)
+        {
+            String[] teacherArray = {"Mrs. Long", "Mrs. Lychock", "Ms. Martin"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+        else if(grade == 1)
+        {
+            String[] teacherArray = {"Mrs. Perzan", "Mrs. Pfeffer", "Mrs. Morin"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+        else if(grade == 2)
+        {
+            String[] teacherArray = {"Ms. Holzer", "Mrs. Tomayo", "Mrs. Zino"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+        else if(grade == 3)
+        {
+            String[] teacherArray = {"Mrs. Montero", "Ms. Rodriguez", "Mrs. Dansby"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+        else if(grade == 4)
+        {
+            String[] teacherArray = {"Ms. Grunloh", "Ms. MacInness"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(2));
+        }
+        else if(grade == 5)
+        {
+            String[] teacherArray = {"Ms. Rios", "Ms. Kirby", "Mrs. Wilson"};
+            List<String> teacherList = Arrays.asList(teacherArray);
+            teacher = teacherList.get(generator.nextInt(3));
+        }
+    }
+
 }
+
+//else if (grade == 5)
+//        {
+//        String[] teacherArray = {"", ""};
+//        List<String> teacherList = Arrays.asList(teacherArray);
+//        teacher = teacherList.get(generator.nextInt());
+//        }
+
+    //Take a school from dropdown
+    //Then based on which level it is (Elem, Middle, High), will generate a grade
+    //Then based on the grade it selects will randomize a specific teacher
+    /* So What Do We Need Now:
+     * This class will take school value from the panel-- and with it run it through the available levels
+     * There will be a method that takes the school option (go through each one in the dropdown) and if it
+     * one of the certain options in each if/else statement, it will set the variable of level
+     * The level is Elem, Middle, High. Based on this it will randomize a grade (except for high school)
+     * In the high school format I want to pick STEM specific topics. Based on the grade and school it will then
+     * randomize a teacher.
+     *
+     */
+
+
